a3ed58f2515ae41d8da2e3c4bc829a8cc90e9668
HisatakaSuzuki <affection.u.was@gmail.com>
2017-12-11 23:58:12 +0900

    Textがクリックに反応するように設定

---
+++ Unity/Assets/Scripts/LotteryNumber.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class LotteryNumber : MonoBehaviour{
+	public bool isAtari;
+	public string objectName;
+
+	// Use this for initialization
+	void Start () {
+		isAtari = false;
+		objectName = this.gameObject.name;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void OnSelect(){
+		isAtari = true;
+	}
+}
