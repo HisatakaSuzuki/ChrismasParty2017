cc24eb2692a2b184bca6151b3fbd3545abb05781
HisatakaSuzuki <affection.u.was@gmail.com>
2017-12-11 22:30:14 +0900

    数字の表示の仕方を変えるための準備

--- Unity/Assets/Scripts/LotterySystem.cs
+++ Unity/Assets/Scripts/LotterySystem.cs
@@ -7,28 +7,49 @@ using System.IO;
 
 
 public class LotterySystem : MonoBehaviour {
+	public GameObject lotteryNumberObject;
+	GameObject[] shownNumbers;
     Text lotteryText;
 	Color textColor;
 
     public float sleepTime = 0.1f;
     public float seed = 1.0f;
 
+	bool showFlag;
+
 	// Use this for initialization
 	void Start () {
-        lotteryText = this.gameObject.GetComponent<Text>();
-		textColor = new Color (.0f,.0f,.0f,.0f);
-		lotteryText.color = textColor;
+		showFlag = false;
 	}
 
 	// Update is called once per frame
 	void Update () {
-		if (GameManager.state == GameManager.STATE.Lottery) {//抽選
-			textColor.a = 1.0f;
-			lotteryText.color = textColor;
+		if (GameManager.state == GameManager.STATE.Wait) {
+			if (!showFlag) {
+				shownNumbers = new GameObject[2];
+				for (int i = 0; i < shownNumbers.Length; i++) {
+					shownNumbers [i] = Instantiate (lotteryNumberObject, this.transform) as GameObject;
+					StartCoroutine(ShowNumber (i, 100 * i));
+				}
+				showFlag = true;
+			}
+		}
+		else if (GameManager.state == GameManager.STATE.Lottery) {//抽選
 			StartCoroutine ("Lottery");
 		} else if(GameManager.state == GameManager.STATE.PresentShow || GameManager.state == GameManager.STATE.End){
-			textColor.a = 0.0f;
-			lotteryText.color = textColor;
+			showFlag = false;
+		}
+	}
+
+	IEnumerator ShowNumber(int i, float distY){
+		Text t = shownNumbers [i].GetComponent<Text> ();
+		for (; ; ) {
+			yield return new WaitForSeconds (0.01f);
+			Color c = new Color (.0f, .0f, .0f, t.color.a + 0.01f);
+			t.color = c;
+			if (t.color.a >= 0.8f) {
+				break;
+			}
 		}
 	}
 
